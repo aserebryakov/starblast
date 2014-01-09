6cf8978edf9c422f5edce2faed1193a4a0c320ed
Alexander Serebryakov <alex.serebr@gmail.com>
2014-01-09 21:00:40 +0200

    This commit changes enemy generation probablility

--- GameSettings.py
+++ GameSettings.py
@@ -4,11 +4,11 @@ from pygame.locals import *
 class Colors:
     BLACK = pygame.Color(0,0,0)
     WHITE = pygame.Color(255, 255, 255)
-    BLUE  = pygame.Color(60, 60, 255)
+    BLUE  = pygame.Color(60, 60, 200)
 
 class GameSettings:
     SCREENWIDTH = 800
     SCREENHEIGHT = 600
     BACKGROUND_COLOR = Colors.BLUE
     FPS = 30
-    ENEMY_CHANCE = 0.3
+    ENEMY_CHANCE = 0.2
